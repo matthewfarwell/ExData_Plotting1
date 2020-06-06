4684575b36c6a9f224b655194ee02200e036f455
Matthew Farwell <matthew.farwell@nexthink.com>
2020-06-07 00:08:11 +0200

    Fourth plot

---
+++ plot4.R
@@ -0,0 +1,21 @@
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep = ";", dec = ".")
+hpcdates <- subset(hpc, Date == "1/2/2007" | Date == "2/2/2007")
+hpcdates$Date_and_Time = strptime(paste(hpcdates$Date, " ", hpcdates$Time), "%d/%m/%Y %H:%M:%S")
+
+png(file = "plot4.png", bg = "white", width = 480, height = 480)
+
+par(mfcol = c(2,2))
+
+plot(hpcdates$Date_and_Time, hpcdates$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+
+plot(hpcdates$Date_and_Time, hpcdates$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering", col = "grey25")
+lines(hpcdates$Date_and_Time, hpcdates$Sub_metering_2, type = "l", col = "red")
+lines(hpcdates$Date_and_Time, hpcdates$Sub_metering_3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("grey25", "red", "blue"), box.lty = 0)
+
+plot(hpcdates$Date_and_Time, hpcdates$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+plot(hpcdates$Date_and_Time, hpcdates$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+
+dev.off()
